a3868b4f316e7569d9fe8bcaec25b8f7058c505e
Taman-07 <tamanjot707@gmail.com>
2025-12-03 20:10:00 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express=require('express');
+
+const app=express();
+
+app.use("/test", (req, res) =>{
+    res.send("Hello from the server ");
+})
+
+app.use("/hi", (req, res) =>{
+    res.send("Hello");
+})
+
+app.listen(3000, () => {
+    console.log("Server is listening");
+});
+
