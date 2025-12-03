109c63856f993e8de8f0730aa850c9596bfcd14f
Taman-07 <tamanjot707@gmail.com>
2025-12-03 21:00:31 +0530

    exploring get,post and delete

--- src/app.js
+++ src/app.js
@@ -2,13 +2,26 @@ const express=require('express');
 
 const app=express();
 
+
+app.get("/user", (req, res)=> {
+    res.send({"firstname":"Tamanjot", "lastname":"Kaur"});
+})
+
+app.post("/user", (req, res)=> {
+    res.send("Data successully saved to the database");
+})
+
+app.delete("/user", (req, res)=> {
+    res.send("Delete user successfully");
+})
+
+// order matters if we write the "/" in the first then it completes all the requests
+//.use match all the https call methods to /test
+
 app.use("/test", (req, res) =>{
     res.send("Hello from the server ");
 })
 
-app.use("/hi", (req, res) =>{
-    res.send("Hello");
-})
 
 app.listen(3000, () => {
     console.log("Server is listening");
