426b57aca01b5ec863b698af3566b6b8e76c1c78
Taman-07 <tamanjot707@gmail.com>
2025-12-06 16:18:36 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -3,24 +3,47 @@ const express=require('express');
 const app=express();
 
 
-app.get("/user", (req, res)=> {
-    res.send({"firstname":"Tamanjot", "lastname":"Kaur"});
-})
+app.use("/user", [(req, res, next)=> {
+    console.log("Response 1");
+    // res.send("Response1 !!!!!");
+    next();
+},
+    (req,res,next)=>{
+        console.log("Response 2");
+        // res.send("Response 2!!!!");
+        next();
+    },],
+    (req,res)=>{
+        console.log("Response 3");
+        res.send("Response 3!!!!");
+    }
+)
 
-app.post("/user", (req, res)=> {
-    res.send("Data successully saved to the database");
-})
 
-app.delete("/user", (req, res)=> {
-    res.send("Delete user successfully");
-})
 
-// order matters if we write the "/" in the first then it completes all the requests
-//.use match all the https call methods to /test
+// app.get("/user", (req, res)=> {
+//     res.send({"firstname":"Tamanjot", "lastname":"Kaur"});
+// })
 
-app.use("/test", (req, res) =>{
-    res.send("Hello from the server ");
-})
+
+// app.get("/user/:name", (req, res)=> {
+//     res.send({"firstname":"Tamanjot", "lastname":"Kaur"});
+// })
+
+// app.post("/user", (req, res)=> {
+//     res.send("Data successully saved to the database");
+// })
+
+// app.delete("/user", (req, res)=> {
+//     res.send("Delete user successfully");
+// })
+
+// // order matters if we write the "/" in the first then it completes all the requests
+// //.use match all the https call methods to /test
+
+// app.use("/test", (req, res) =>{
+//     res.send("Hello from the server ");
+// })
 
 
 app.listen(3000, () => {
