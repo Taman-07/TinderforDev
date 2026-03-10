8f8eac9710faaee72d90cb5de43bcdd3fdb0c191
Taman-07 <tamanjot707@gmail.com>
2026-03-10 20:11:34 +0530

    Changes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,6 @@
+const jwt=require("jsonwebtoken");
+const User =require("../models/user");
+
 const adminAuth = (req,res,next) =>{
     // logic for the authorization
     console.log("Admin auth is getting checked !");
@@ -12,17 +15,79 @@ const adminAuth = (req,res,next) =>{
     }
 };
 
-const userAuth = (req,res,next) =>{
-    // logic for the authorization
-    console.log("Admin auth is getting checked !");
+// const userAuth = (req,res,next) =>{
+//     // logic for the authorization
+//     console.log("Admin auth is getting checked !");
     
-    const token="xyz";
-    const isAdminAuthorized=token=="xyz";
-    if(!isAdminAuthorized){
-        res.status(401).send("Unauthorized Access");
-    }
-    else{
+//     const token="xyz";
+//     const isAdminAuthorized=token=="xyz";
+//     if(!isAdminAuthorized){
+//         res.status(401).send("Unauthorized Access");
+//     }
+//     else{
+//         next();
+//     }
+// };
+
+
+
+// const userAuth = async (req,res,next) =>  {
+//     // read token from the request cookies 
+//     // validate token
+//     // find the user
+
+
+//     try{
+//         const cookies=req.cookies;
+//         const { token }=req.cookies;
+
+//         if(!token){
+//             throw new Error("Token is not valid ❗");
+//         }
+//         const decodedObj=await jwt.verify(token, "DEV@Tinder$790");
+
+//         const { _id }=decodedObj;
+
+//         const user=await User.findById(_id);
+
+//         if(!user){
+//             throw new Error("User not found❗");
+//         }
+//         req.user=user;
+//         next();
+//     }
+//     catch(err){
+//         res.status(404).send("Error❗"+ err.message);
+//     }
+// };
+
+const userAuth = async (req,res,next) => {
+
+    try{
+
+        const { token } = req.cookies;
+
+        if(!token){
+            throw new Error("Token is not valid ❗");
+        }
+
+        const decodedObj = jwt.verify(token, "DEV@Tinder$790");
+
+        const { _id } = decodedObj;
+
+        const user = await User.findById(_id);
+
+        if(!user){
+            throw new Error("User not found ❗");
+        }
+
+        req.user = user;
+
         next();
+
+    }
+    catch(err){
+        res.status(401).send("Error ❗ " + err.message);
     }
 };
 
